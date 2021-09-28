f0d1a3dd0b64ca6abb702fee1a3abdc902ef1aa3
Roccofranchini <rocco.franchini1@gmail.com>
2021-09-28 16:52:52 +0200

    qg-3

--- queries.sql
+++ queries.sql
@@ -56,7 +56,9 @@ GROUP BY `office_address`;
 
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT round(AVG(`vote`), 2) AS 'media', `exam_id`
+FROM `exam_student`
+GROUP BY `exam_id`;
 
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
