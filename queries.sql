2c8caa8231ec8a25fa7e43a588f1a30a01e3c9e2
Roccofranchini <rocco.franchini1@gmail.com>
2021-09-28 15:57:26 +0200

    q3

--- queries.sql
+++ queries.sql
@@ -7,3 +7,10 @@ SELECT cfu
 FROM `courses`
 WHERE cfu >= 10
 ORDER BY `cfu` ASC;
+
+--3 selezionare tutti gli studernti che hanno più di 30 anni
+SELECT `date_of_birth` 
+FROM `students` 
+WHERE date_of_birth <= '1991-09-28'
+ORDER BY `date_of_birth` DESC;
+
