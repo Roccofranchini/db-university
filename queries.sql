f0a01ca8b5bc829727499f9e81098611ea9e0739
Roccofranchini <rocco.franchini1@gmail.com>
2021-09-28 16:04:00 +0200

    q4

--- queries.sql
+++ queries.sql
@@ -1,16 +1,20 @@
--- 1. selezionare tutti gli studenti nati nel 1990
+-- 1. Selezionare tutti gli studenti nati nel 1990 (160)
 SELECT date_of_birth
 FROM `students` WHERE YEAR(date_of_birth) = 1990;
 
--- 2. selezionare tutti i corsi che valgono più di 10 crediti
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 SELECT cfu
 FROM `courses`
 WHERE cfu >= 10
 ORDER BY `cfu` ASC;
 
---3 selezionare tutti gli studernti che hanno più di 30 anni
+--  3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT `date_of_birth` 
 FROM `students` 
 WHERE date_of_birth <= '1991-09-28'
 ORDER BY `date_of_birth` DESC;
 
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT *
+FROM `courses`
+WHERE `period`= 'I semestre' AND `year` = 1;
