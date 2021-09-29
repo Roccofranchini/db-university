a51fe3c2df5763aa4e7b68c99cdd035e019ac8df
Roccofranchini <rocco.franchini1@gmail.com>
2021-09-29 18:34:18 +0200

    jq-4

--- join-queries.sql
+++ join-queries.sql
@@ -23,7 +23,13 @@ WHERE `teachers`.`name` = "Fulvio"
 AND `teachers`.`surname`= "Amato"
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
-
+SELECT *
+FROM `students`
+JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`name`, `students`.`surname` ASC
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
