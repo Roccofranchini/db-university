ab62068c7beda8a4ac6cc5b4417682f62967acb3
Roccofranchini <rocco.franchini1@gmail.com>
2021-09-29 17:53:13 +0200

    jq-3

--- join-queries.sql
+++ join-queries.sql
@@ -13,6 +13,12 @@ ON `degrees`.`department_id` = `departments`.`id`
 WHERE `departments`. `name` = "Dipartimento di Neuroscienze"
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT * 
+FROM `course_teacher`
+JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.`id`
+WHERE `course_teacher`.`teacher_id` = 44
+
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
